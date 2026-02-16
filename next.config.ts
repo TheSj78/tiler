590519a24a763e74e1fec14eada698eff2d4146b
TheSj78 <zoom2shubham@gmail.com>
2026-02-15 22:48:45 -0500

    Fix config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,7 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   output: 'export',
-//   reactCompiler: true,
-//   basePath: '/tiler', 
-//   images: {
-//     unoptimized: true,
-//   }
-// };
-
-// export default nextConfig;
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = 'tiler';
-
 const nextConfig: NextConfig = {
-  output: 'export',
   
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
-
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
