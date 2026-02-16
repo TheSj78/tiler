9f2faedd71afd587b780e4cb76a3c67bf747335d
TheSj78 <zoom2shubham@gmail.com>
2026-02-15 22:37:08 -0500

    Updates

--- _next/static/bX0d_YMJnREYfOsxRVSN8/_buildManifest.js
+++ _next/static/bX0d_YMJnREYfOsxRVSN8/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/tiler//_next/:path+",
-        "destination": "/tiler/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
