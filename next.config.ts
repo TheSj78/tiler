73b46a4fac4e537ff8416ed94f5646e0f6981fc2
TheSj78 <zoom2shubham@gmail.com>
2026-02-15 22:25:51 -0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,30 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: 'export',
+//   reactCompiler: true,
+//   basePath: '/tiler', 
+//   images: {
+//     unoptimized: true,
+//   }
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'tiler';
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'export',
-  reactCompiler: true,
+  
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
